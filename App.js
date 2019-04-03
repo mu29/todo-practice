d5104795880515917bb1b080d6679c40bb6a2928
정인중 [N Studio] <mu29@snowcorp.com>
2019-04-04 08:28:33 +0900

    5-1. 정보 관리 맛보기

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, FlatList } from 'react-native';
 import Header from './components/Header'
 import TodoItem from './components/TodoItem'
 import TodoModal from './components/TodoModal'
 
 export default class App extends React.Component {
+  state = {
+    todos: [{
+      title: '클래스101 커리큘럼 만들기',
+      done: true,
+    }, {
+      title: '운전면허 도로 주행 연수',
+      done: false,
+    }],
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <Header />
-        <TodoItem title="클래스101 수업 촬영" done={true} />
-        <TodoItem title="운전면허 도로연수" done={false} />
+        <FlatList
+          data={this.state.todos}
+          renderItem={({ item }) => {
+            return (<TodoItem title={item.title} done={item.done} />)
+          }}
+          keyExtractor={(_, index) => `${index}`}
+        />
         <TodoModal isVisible={false} />
       </SafeAreaView>
     );
