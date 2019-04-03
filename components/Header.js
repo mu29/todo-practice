11ab76d17a95a17e378b1e4b7b9bcc42749135b0
정인중 [N Studio] <mu29@snowcorp.com>
2019-04-04 07:38:30 +0900

    4-4. 할 일 추가 부품 만들기

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,11 @@ import React from 'react'
 import {
   View,
   Text,
+  TouchableOpacity,
   StyleSheet,
+  Platform,
 } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
 const Header = () => {
   return (
@@ -11,21 +14,45 @@ const Header = () => {
       <Text style={styles.title}>
         할 일 목록
       </Text>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={styles.button}
+      >
+        <Ionicons
+          name="ios-add"
+          color="#FFFFFF"
+          size={24}
+        />
+      </TouchableOpacity>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     marginTop: 56,
     marginBottom: 16,
     marginLeft: 16,
+    marginRight: 16,
   },
   title: {
     color: '#212121',
     fontSize: 32,
     fontWeight: '600',
   },
+  button: {
+    width: 28,
+    height: 28,
+    borderRadius: 14,
+    backgroundColor: '#212121',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: Platform.select({ ios: 2, android: 0 }),
+    paddingLeft: Platform.select({ ios: 1, android: 0 }),
+  }
 })
 
 export default Header
